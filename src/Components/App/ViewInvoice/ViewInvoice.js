2d1ccffef612df56f1f9129e398348ccda226157
abelmuro <abelmuro93@gmail.com>
2023-04-25 11:38:50 -0700

    commit

--- src/Components/App/ViewInvoice/ViewInvoice.js
+++ src/Components/App/ViewInvoice/ViewInvoice.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import styles from './styles.module.css';
 import icons from './icons';
-
+import {useNavigate} from 'react-router-dom';
 
 //this is where i left off, i will need to style the buttons and the text in the styles.invoiceDetails
 function ViewInvoice() {
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate('/');
+    }
+
     return(
         <section className={styles.invoice}>
-            <a className={styles.goBackLink}>
+            <a className={styles.goBackLink} onClick={handleGoBack}>
                 <img src={icons['arrowLeft']} className={styles.arrow}/>
                 Go back
             </a>
             <div className={styles.invoice_details}>
-                <div className={styles.invoice_statusTitle}>
+                <div className={styles.invoice_statusTitle}>          
                     Status
                     <div className={styles.invoice_status}>
                         <div className={styles.dot}></div>
@@ -30,7 +36,24 @@ function ViewInvoice() {
                         Mark as Paid
                     </button>
                 </div>
-
+            </div>
+            <div className={styles.invoice_content}>
+                <div className={styles.invoice_header}>
+                    <h3 className={styles.invoice_title_desc}>
+                        <span className={styles.invoice_title}>
+                            <span>#</span>XM7845
+                        </span>
+                        <span className={styles.invoice_title_desc}>
+                            Graphic Design
+                        </span>
+                    </h3>
+                    <p className={styles.invoice_address}>
+                        19 Union Terrace<br/>
+                        London<br/>
+                        E1 3EZ<br/>
+                        United Kingdom<br/>
+                    </p>
+                </div>
             </div>
 
         </section>
